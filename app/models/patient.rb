51312c4dd90690aa42d855391ac580905eeb7750
Daniel Gilbert <jjb2016@hotmail.com>
2018-04-29 16:02:30 +0100

    Relationships update

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,4 @@
 class Patient < ApplicationRecord
   has_many :records
-  def self.search(search)
-  if search
-    where('name LIKE ?', "%#{search}%")
-  else
-  end
-end
+    validates :name, :address, :phone_no , :dob , presence: true
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,4 @@
 class Record < ApplicationRecord
   belongs_to :patient, dependent: :destroy
+    validates :observation, :clinic, presence: true
 end
